ac6613f46f156463ce9e6466461bef95019b9808
谢栩 <xiexu@dslyy.com>
2026-01-10 09:10:51 +0800

    feat[删除冗余文件]

--- docs/dev-ops/api/curl.sh
+++
@@ -1,7 +0,0 @@
-curl http://192.168.110.112:11434/api/generate \
-  -H "Content-Type: application/json" \
-  -d '{
-        "model": "deepseek-r1:1.5b",
-        "prompt": "1+1",
-        "stream": false
-      }'
